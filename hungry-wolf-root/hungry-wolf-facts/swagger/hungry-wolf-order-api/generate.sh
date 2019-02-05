e239a429229032a084c00acfb589b4a09bf96a1b
deepikabskrn <deepikabskrn@gmail.com>
2019-02-04 19:16:02 -0800

    recent swaggger changes

---
+++ hungry-wolf-root/hungry-wolf-facts/swagger/hungry-wolf-order-api/generate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#rm -r generated
+mkdir -r generated
+java -jar ../swagger-codegen-cli-2.4.1.jar generate \
+  -v -i hungry-wolf-order-api.yaml \
+  -l spring --library spring-boot \
+  -o generated \
+  --import-mappings ZonedDateTime=java.time.ZonedDateTime \
+  --type-mappings OffsetDateTime=ZonedDateTime \
+  -DmodelPackage=com.hungrywolf.facts.model.generated \
+  -DapiPackage=com.hungrywolf.feed.api.controller -Djava8=true -DdateLibrary=java8 -DuseBeanValidation=true -DperformBeanValidation=true
